cf755330a584a39b0e1e2e063a0f5d84a4325a9b
psu6510110233 <6510110233@email.psu.ac.th>
2023-01-19 00:05:08 +0700

    ดึง Public key

--- server-ts/src/auth.ts
+++ server-ts/src/auth.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import appConfig from "config"
+import appConfig from "./config"
 import jwt from 'jsonwebtoken'
 import Koa from 'koa'
 
@@ -10,12 +10,24 @@ export interface AuthData {
     isStaff: boolean
 }
 
-export async function  initSsoCert() {
+export async function initSsoCert() {
     const result = await axios.get(appConfig.ssoIssuer)
     public_key = `-----BEGIN PUBLIC KEY-----\n${result.data.public_key}\n-----END PUBLIC KEY-----\n`
 }
 
 export async function verifyJwt(token: string){
+    if(appConfig.isDev && token.startsWith('DEV::')){
+        const items = token.split('::')
+        const data =  {
+          preferred_username: items[1],
+          groups: ['student']
+        }
+        if(items.length >= 3 && items[2] == '1'){
+          data.groups.push('staff')
+        }
+        return data
+      }
+
     return new Promise<any>((resolve, reject) => {
         jwt.verify(token, public_key as jwt.Secret, { issuer: appConfig.ssoIssuer, algorithms: ['RS256'] },(err,decoded) => {
             if(err){
@@ -34,14 +46,14 @@ export async function authMiddleware(ctx: Koa.Context, next: () => Promise<any>)
         const token = bearer.substring(BEARER_PREFIX.length)
         const decoded = await verifyJwt(token)
         const authData: AuthData = {
-            username: decoded.prefered_username as string,
+            username: decoded.preferred_username as string,
             isStaff: (decoded.groups as string[])?.indexOf('staff') >= 0
         }
         ctx.state.authData = authData
         await next()
         return
     }
-    ctx.response.status = 401    
+    ctx.response.status = 401
 }
 
 export default {}
--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,16 +1,20 @@
 import Koa from 'koa'
-import json from 'koa-json'
 import apiRouter from './api'
 import appConfig from './config'
 import loadFixtures from './fixture'
 import { koaBody } from 'koa-body'
+import { initSsoCert } from './auth'
+import cors from '@koa/cors'
 
 const app = new Koa()
 
-app.use(json())
-app.use(koaBody())
-app.use(apiRouter.routes())
+app.use(cors());
+app.use(koaBody());
+app.use(apiRouter.routes());
 
-app.listen(8000)
-
-loadFixtures(appConfig.ClearDataBeforeLoad)
+(async () => {
+    await loadFixtures(appConfig.ClearDataBeforeLoad)
+    await initSsoCert()
+    app.listen(8000)
+    console.log('Server is ready at port 8000')
+})();
