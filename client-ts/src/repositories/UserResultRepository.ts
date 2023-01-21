3b5342e3b082404d96b384f09a9f2d24078bb1f4
psu6510110233 <6510110233@email.psu.ac.th>
2023-01-21 15:39:44 +0700

    สร้าง user-result-list และ user-result-card

--- client-ts/src/repositories/UserResultRepository.ts
+++ client-ts/src/repositories/UserResultRepository.ts
@@ -17,4 +17,34 @@ export class UserResultRepotiory implements IRepository<UserResult> {
         return resp.data
 
     }
+    async get(id: string|number): Promise<UserResult | null> {
+        const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}`)        
+        return resp.data 
+    }
+
+    async create(entity: Partial<UserResult>): Promise<UserResult | null> {
+        const resp = await ax.post<UserResult>(`${this.urlPrefix}/userResult`, entity)    
+        return resp.data
+    }
+
+    async update(entity: Partial<UserResult>): Promise<UserResult | null> {
+        const resp = await ax.put<UserResult>(`${this.urlPrefix}/userResult/${entity.id}`, entity)
+        return resp.data
+    }
+
+    async view(id: string|number): Promise<UserResult | null> {
+        const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}/markAsViewed`)
+        return resp.data
+    }
+
+    async acknowledge(id: string|number): Promise<UserResult | null> {
+        const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}/acknowledge`)
+        return resp.data
+    }
+
+    async toggleIsPinned(id: string|number): Promise<UserResult | null> {
+        const resp = await ax.get<UserResult>(`${this.urlPrefix}/userResult/${id}/pin`)
+        return resp.data
+    }
+    
 }
